dcdf1928b618c9e4734459ca1e2fc651734cef36
Bassem M. Aly <bassemaly12@gmail.com>
2025-08-04 04:11:05 +0300

    Update sources_manipulation.py

--- api/sources_manipulation.py
+++ api/sources_manipulation.py
@@ -1,4 +1,4 @@
-from api.extract_content_from_website import extract_website_content
+from .extract_content_from_website import extract_website_content
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
